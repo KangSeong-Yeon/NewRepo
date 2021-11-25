1b403b79553160a11fe955a9a136fea920a43caf
성연 강 <rkdtjddus11@naver.com>
2021-11-25 23:44:30 +0900

    최종 코드

--- prg/prg/g.c
+++ prg/prg/g.c
@@ -1,415 +1,258 @@
 // 프로젝트
 // 별 피하기 게임(Avoiding the Stars Game)
 
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <Windows.h>
-#include <stdbool.h>
+#include <windows.h>
 #include <conio.h>
+#include <time.h>
+
+#define MAX_STAR    90  // 별이 떨어질 수 있는 최대 개수
+#define LEVEL_COUNT    3  // 난이도 상승시 별의 출력 개수 증가량
+#define LEVEL_SPEED    15  // 난이도 상승시 별의 떨어지는 속도 증가량
+
+// 별 구조체
+typedef struct {
+    int x;
+    int y;
+    int wait;
+} STAR;
+
+// 플레이어 구조체
+typedef struct {
+    int x;
+    int y;
+} PLAYER;
 
 int game_x, game_y;  // 화면 구성 변수
 char name[50];  // 플레이어 이름 변수
-int player_x, player_y;  // 플레이어 위치 변수
-int key;  // 키보드 방향키 입력 변수
-// 별 위치 변수
-int star1_x, star1_y, star2_x, star2_y, star3_x, star3_y;
-int star4_x, star4_y, star5_x, star5_y;
-int star6_x, star6_y, star7_x, star7_y, star8_x, star8_y, star9_x, star9_y, star10_x, star10_y;
-int avoid_star;  // 피한 별 개수 변수
 char test[100];  // 한/영키 테스트 변수
+int score = 0;  // 점수 변수
 
 void start();  // 게임 시작 전 문구 출력 함수
-void initialize();  // 변수 초기화 함수
-void display();  // 게임판 출력 함수
-void level_easy();  // 쉬움 난이도 실행 함수
-void level_normal();  // 중간 난이도 실행 함수
-void level_hard();  // 어려움 난이도 실행 함수
-void easy_falling_star();  // 쉬움 난이도 별 떨어짐 함수
-void normal_falling_star();  // 중간 난이도 별 떨어짐 함수
-void hard_falling_star();  // 어려움 난이도 별 떨어짐 함수
+int game(int);  // 게임 실행 함수
+
+// 화면의 특정 위치로 커서를 이동시키는 함수
+void gotoxy(int x, int y)
+{
+    COORD Pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
 
 void start() {
-	printf("===========================================\n");
-	printf("  별 피하기 게임(Avoiding the Stars Game)  \n");
-	printf("===========================================\n");
-	printf("\n");
+    printf("=============================================\n");
+    printf("=  별 피하기 게임(Avoiding the Stars Game)  =\n");
+    printf("=============================================\n");
+    printf("\n");
 
-	// 플레이어 이름 입력
-	printf("플레이어의 이름을 입력해주세요. >> ");
-	scanf_s("%s", name, 50);
+    // 플레이어 이름 입력
+    printf("플레이어의 이름을 입력해주세요. >> ");
+    scanf_s("%s", name, 50);
 
-	// 게임 안내 문구 출력
-	printf("\n%s 님! 안녕하세요.\n", name);
-	Sleep(1500);
-	printf("이 게임은 하늘에서 떨어지는 별들을 부딪히지 않고 피하면 되는 게임입니다.\n");
-	Sleep(1500);
-	printf("일정 점수에 도달하게 되면 난이도가 상승하여 별의 개수가 늘어나고 떨어지는 속도가 빨라집니다.\n");
-	Sleep(1500);
-	printf("'a'키 또는 'A'키를 눌러 왼쪽으로 이동할 수 있으며, 'd'키 또는 'D'키를 눌러 오른쪽으로 이동할 수 있습니다.\n");
-	Sleep(1500);
-	printf("단, 한/영 입력키가 한글로 설정되어있을 시 게임 작동이 원활하게 진행되지 않을 수 있습니다.\n");
-	Sleep(1500);
-	printf("게임 시작 전 테스트를 통해 한글 입력 상태라면 한/영 입력키를 영어 입력 상태로 설정하여 주십시오.\n>> ");
-	scanf_s("%s", test, 100);
-	printf("\n좋습니다!\n");
-	Sleep(1500);
-	printf("그럼 5초의 카운트 후 게임을 시작하겠습니다.\n");
-	Sleep(1500);
-	printf("행운을 빕니다! GOOD LUCK!\n");
-	Sleep(1000);
-	printf("\n");
+    // 게임 안내 문구 출력
+    printf("\n%s 님! 안녕하세요.\n", name);
+    Sleep(1500);
+    printf("이 게임은 하늘에서 떨어지는 별들을 부딪히지 않고 피하면 되는 게임입니다.\n");
+    Sleep(1500);
+    printf("일정 개수의 별을 피하게 되면 난이도가 상승하여 별의 개수가 늘어나고 떨어지는 속도가 빨라집니다.\n");
+    Sleep(1500);
+    printf("'a'키 또는 'A'키를 눌러 왼쪽으로 이동할 수 있으며, 'd'키 또는 'D'키를 눌러 오른쪽으로 이동할 수 있습니다.\n");
+    Sleep(1500);
+    printf("단, 한/영 입력키가 한글로 설정되어있을 시 게임 작동이 원활하게 진행되지 않을 수 있습니다.\n");
+    Sleep(1500);
+    printf("게임 시작 전 테스트를 통해 한글 입력 상태라면 한/영 입력키를 영어 입력 상태로 설정하여 주십시오.\n>> ");
+    scanf_s("%s", test, 100);
+    printf("\n좋습니다!\n");
+    Sleep(1500);
+    printf("그럼 게임을 시작하겠습니다.\n");
+    Sleep(1500);
+    printf("행운을 빕니다! GOOD LUCK!\n");
+    Sleep(1000);
 
-	// 게임 시작 카운트 다운 표시
-	for (int i = 5; i > 0; i--) {
-		printf("%d\n", i);
-		Sleep(900);
-	}
-
-	system("cls");
+    system("cls");
 }
 
-void initialize() {
-	// 플레이어 초기 위치
-	player_x = 5;
-	player_y = 8;
+int game(int level)
+{
+    STAR star[MAX_STAR];  // 별 위치 변수
+    PLAYER player;  // 플레이어 위치 변수
+    int key;  // 키보드 방향키 입력 변수
+    int check = 1;  // 게임 진행 체크 변수
+    int avoid_star = 0;  // 피한 별 개수 변수
+    int speed = 200 - level * LEVEL_SPEED;  // 별 떨어지는 속도 변수
+    int count = level * LEVEL_COUNT;  // 별 출력 개수 변수
 
-	// 별 x좌표 초기 위치
-	star1_x = rand() % 3 + 1;
-	star2_x = rand() % 3 + 4;
-	star3_x = rand() % 3 + 7;
-	star4_x = rand() % 5 + 1;
-	star5_x = rand() % 5 + 5;
-	star6_x = rand() % 4 + 1;
-	star7_x = rand() % 4 + 6;
-	star8_x = rand() % 5 + 3;
-	star9_x = rand() % 2 + 1;
-	star10_x = rand() % 2 + 8;
+    // 난이도 상승에 필요한 별의 개수
+    int levelup = 20 + level * 20;
 
-	// 별 y좌표 초기 위치
-	star1_y = (rand() % 3) - 4;
-	star2_y = (rand() % 3) - 4;
-	star3_y = (rand() % 3) - 4;
-	star4_y = (rand() % 3) - 4;
-	star5_y = (rand() % 3) - 4;
-	star6_y = (rand() % 3) - 4;
-	star7_y = (rand() % 3) - 4;
-	star8_y = (rand() % 3) - 4;
-	star9_y = (rand() % 3) - 4;
-	star10_y = (rand() % 3) - 4;
+    // 별의 출력 개수가 최댓값을 넘지 않도록 조정
+    if (count >= MAX_STAR) {
+        count = MAX_STAR - 1;
+    }
 
-	// 피한 별 개수 초기화
-	avoid_star = 0;
-}
+    // 별의 속도의 최솟값이 10보다 작지 않도록 조정
+    if (speed < 10) {
+        speed = 10;
+    }
 
-void display() {
-	// 게임판 출력
-	for (game_y = 0; game_y < 10; game_y++) {
-		for (game_x = 0; game_x < 11; game_x++) {
-			// 게임판 벽 출력
-			if (game_x == 0 || game_x == 10) {
-				printf("#");
-			}
-			else if (game_y == 9) {
-				printf("##");
-			}
+    // 플레이어 시작 위치
+    player.x = 9;
+    player.y = 10;
 
-			// 별 출력
-			else if (game_x == star1_x && game_y == star1_y) {
-				printf("★");
-			}
-			else if (game_x == star2_x && game_y == star2_y) {
-				printf("★");
-			}
-			else if (game_x == star3_x && game_y == star3_y) {
-				printf("★");
-			}
-			else if (game_x == star4_x && game_y == star4_y) {
-				printf("★");
-			}
-			else if (game_x == star5_x && game_y == star5_y) {
-				printf("★");
-			}
-			else if (game_x == star6_x && game_y == star6_y) {
-				printf("★");
-			}
-			else if (game_x == star7_x && game_y == star7_y) {
-				printf("★");
-			}
-			else if (game_x == star8_x && game_y == star8_y) {
-				printf("★");
-			}
-			else if (game_x == star9_x && game_y == star9_y) {
-				printf("★");
-			}
-			else if (game_x == star10_x && game_y == star10_y) {
-				printf("★");
-			}
+    // 별 위치 초기화
+    for (int i = 0; i < count; i++)
+    {
+        star[i].x = rand() % 19;
+        star[i].y = 1;
+        star[i].wait = rand() % 10;
+    }
 
-			// 플레이어 출력
-			else if (game_x == player_x && game_y == player_y) {
-				printf("○");
-			}
+    // 게임 루프
+    while (check)
+    {
+        // 스피드에 따른 시연 지연.
+        Sleep(speed);
 
-			// 게임판 공백 출력
-			else {
-				printf("  ");
-			}
-		}
-		putchar('\n');
-	}
-}
+        // 화면 지우기
+        system("cls");
 
-void easy_falling_star() {
-	star1_y++;
-	Sleep(20);
-	if (star1_y > 8) {
-		star1_x = rand() % 3 + 1;
-		star1_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+        // 게임 환경 출력
+        printf("= 별 피하기 게임   LEVEL %d =", level);
 
-	star2_y++;
-	Sleep(20);
-	if (star2_y > 8) {
-		star2_x = rand() % 3 + 4;
-		star2_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+        // 방향키 입력
+        while (_kbhit())
+        {
+            do {
+                key = _getch();
+            } while (key == 224);
+            switch (key) {
+            case 'a':
+                player.x--;
+                if (player.x < 0) {
+                    player.x = 0;
+                    break;
+                }
+                else {
+                    continue;
+                }
+            case 'A':
+                player.x--;
+                if (player.x < 0) {
+                    player.x = 0;
+                    break;
+                }
+                else {
+                    continue;
+                }
+            case 'd':
+                player.x++;
+                if (player.x > 19) {
+                    player.x = 19;
+                    break;
+                }
+                else {
+                    continue;
+                }
+            case 'D':
+                player.x++;
+                if (player.x > 19) {
+                    player.x = 19;
+                    break;
+                }
+                else {
+                    continue;
+                }
+            }
+        }
 
-	star3_y++;
-	Sleep(20);
-	if (star3_y > 8) {
-		star3_x = rand() % 3 + 7;
-		star3_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-}
+        // 플레이어 출력
+        gotoxy(player.x, player.y);
+        printf("○");
 
-void normal_falling_star() {
-	star1_y++;
-	Sleep(13);
-	if (star1_y > 8) {
-		star1_x = rand() % 3 + 1;
-		star1_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+        // 별 떨어짐 처리
+        for (int i = 0; i < count; i++)
+        {
+            // 별이 떨어지기 전에 기다리기
+            if (star[i].wait > 0) {
+                star[i].wait--;
+            }
+            else {
+                star[i].y++;
 
-	star2_y++;
-	Sleep(13);
-	if (star2_y > 8) {
-		star2_x = rand() % 3 + 4;
-		star2_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+                // 별이 바닥과 닿았을 때 처리
+                if (star[i].y > 10)
+                {
+                    star[i].x = rand() % 19;
+                    star[i].y = 1;
+                    avoid_star++;
+                    score++;
+                }
 
-	star3_y++;
-	Sleep(13);
-	if (star3_y > 8) {
-		star3_x = rand() % 3 + 7;
-		star3_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+                // 현재 점수 출력
+                gotoxy(0, 12);
+                printf("현재 점수 : %d\n", score);
 
-	star4_y++;
-	Sleep(13);
-	if (star4_y > 8) {
-		star4_x = rand() % 5 + 1;
-		star4_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+                // 별 출력
+                gotoxy(star[i].x, star[i].y);
+                printf("★");
 
-	star5_y++;
-	Sleep(13);
-	if (star5_y > 8) {
-		star5_x = rand() % 5 + 5;
-		star5_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-}
+                // 충돌 체크
+                if (star[i].x == player.x && star[i].y == player.y)
+                {
+                    check = 0;
+                }
+            }
+        }
+        gotoxy(0, 11);
+        printf("#####################");
 
-void hard_falling_star() {
-	star1_y++;
-	Sleep(3);
-	if (star1_y > 8) {
-		star1_x = rand() % 3 + 1;
-		star1_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
+        // 난이도 상승
+        if (avoid_star >= levelup) {
+            return 1;
+        }
+    }
 
-	star2_y++;
-	Sleep(3);
-	if (star2_y > 8) {
-		star2_x = rand() % 3 + 4;
-		star2_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star3_y++;
-	Sleep(3);
-	if (star3_y > 8) {
-		star3_x = rand() % 3 + 7;
-		star3_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star4_y++;
-	Sleep(3);
-	if (star4_y > 8) {
-		star4_x = rand() % 5 + 1;
-		star4_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star5_y++;
-	Sleep(3);
-	if (star5_y > 8) {
-		star5_x = rand() % 5 + 5;
-		star5_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star6_y++;
-	Sleep(3);
-	if (star6_y > 8) {
-		star6_x = rand() % 4 + 1;
-		star6_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star7_y++;
-	Sleep(3);
-	if (star7_y > 8) {
-		star7_x = rand() % 4 + 6;
-		star7_y = (rand() % 3) - 4;
-		avoid_star++;
-	}
-
-	star8_y++;
-	Sleep(3);
-	if (star8_y > 8) {
-		star8_x = rand() % 5 + 3;
-		star8_y = (rand() % 3) - 4;
-	}
-
-	star9_y++;
-	Sleep(3);
-	if (star8_y > 8) {
-		star8_x = rand() % 2 + 1;
-		star8_y = (rand() % 3) - 4;
-	}
-
-	star10_y++;
-	Sleep(3);
-	if (star8_y > 8) {
-		star8_x = rand() % 2 + 8;
-		star8_y = (rand() % 3) - 4;
-	}
-}
-
-void level_easy() {
-	easy_falling_star();
-}
-
-void level_normal() {
-	normal_falling_star();
-}
-
-void level_hard() {
-	hard_falling_star();
+    return 0;
 }
 
 int main(void) {
-	srand((unsigned)time(NULL));
-	initialize();
-	start();
+    srand((unsigned)time(NULL));
+    int level = 1;
+    start();
 
-	// 게임 화면
-	while (1) {
-		system("cls");
-		display();
+    while (1)
+    {
+        system("cls");
 
-		if (avoid_star < 33) {
-			level_easy();
-			printf("= 현재 난이도 : EASY =\n");
-		}
-		else if (avoid_star < 100) {
-			level_normal();
-			printf("= 현재 난이도 : NORMAL =\n");
-		}
-		else {
-			level_hard();
-			printf("= 현재 난이도 : HARD =\n");
-		}
+        // 게임 시작 전 난이도 출력
+        gotoxy(0, 0);
+        printf("=============================================\n");
+        gotoxy(0, 1);
+        printf("=                                           =\n");
+        gotoxy(0, 2);
+        printf("=  별 피하기 게임(Avoiding the Stars Game)  =\n");
+        gotoxy(0, 3);
+        printf("=                                           =\n");
+        gotoxy(0, 4);
+        printf("=                 LEVEL : %d                 =\n", level);
+        gotoxy(0, 5);
+        printf("=                                           =\n");
+        gotoxy(0, 6);
+        printf("=============================================\n");
 
-		// 현재 점수 출력
-		printf("현재 점수 : %d\n", avoid_star);
+        Sleep(1500);
 
-		// 방향키 입력
-		if (_kbhit()) {
-			key = _getch();
+        // 게임 시작
+        if (game(level++) == 0)
+        {
+            gotoxy(0, 13);
+            printf("\n=====================================\n");
+            printf("= 게임 오버! 게임이 종료되었습니다. =\n");
+            printf("===================================\n");
+            break;
+        }
+    }
 
-			switch (key) {
-			case 'a':
-				player_x--;
-				if (player_x < 1) {
-					player_x = 1;
-					break;
-				}
-				else {
-					continue;
-				}
-			case 'A':
-				player_x--;
-				if (player_x < 1) {
-					player_x = 1;
-					break;
-				}
-				else {
-					continue;
-				}
-			case 'd':
-				player_x++;
-				if (player_x > 9) {
-					player_x = 9;
-					break;
-				}
-				else {
-					continue;
-				}
-			case 'D':
-				player_x++;
-				if (player_x > 9) {
-					player_x = 9;
-					break;
-				}
-				else {
-					continue;
-				}
-			}
-		}
+    printf("\n%s 님께서 획득하신 최종 점수는 %d점 입니다.\n", name, score);
 
-		// 별이랑 플레이어랑 부딪히면 게임 종료
-		if ((player_x == star1_x && player_y == star1_y)
-			|| (player_x == star2_x && player_y == star2_y)
-			|| (player_x == star3_x && player_y == star3_y)
-			|| (player_x == star4_x && player_y == star4_y)
-			|| (player_x == star5_x && player_y == star5_y)
-			|| (player_x == star6_x && player_y == star6_y)
-			|| (player_x == star7_x && player_y == star7_y)
-			|| (player_x == star8_x && player_y == star8_y)
-			|| (player_x == star9_x && player_y == star9_y)
-			|| (player_x == star10_x && player_y == star10_y))
-		{
-			printf("\n===================================\n");
-			printf(" 게임 오버! 게임이 종료되었습니다. \n");
-			printf("===================================\n");
-			break;
-		}
-	}
-
-	printf("\n%s 님께서 획득하신 최종 점수는 %d점 입니다.\n", name, avoid_star);
-
-	return 0;
+    return 0;
 }
